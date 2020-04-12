a21ef12f29a16940aac250c93fcdaf07ec9cf3f8
mwamba <kchangala007@gmail.com>
2020-04-12 02:24:10 +0200

    third commit

--- admin/reports.php
+++ admin/reports.php
@@ -1,3 +1,15 @@
+<?php
+ // Initialize the session
+session_start();
+
+//Check if the user is logged in, if not then redirect him to login page
+if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
+    header("location: ../index.php");
+    exit;
+}
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -30,6 +42,10 @@
     </script>
 </head>
 <body>
+<?php
+require_once '../includes/navbar_header.php';
+
+?>
     <div class="wrapper">
         <div class="container-fluid">
             <div class="row">
