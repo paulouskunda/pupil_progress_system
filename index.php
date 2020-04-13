eec64605201ce4dda3afe61408080aedecdd4d91
mwamba <kchangala007@gmail.com>
2020-04-13 13:31:39 +0200

    removed password verify

--- index.php
+++ index.php
@@ -52,7 +52,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
                     // Bind result variables
                     $stmt->bind_result($id,$username, $hashed_password);
                     if($stmt->fetch()){
-                        if(password_verify($password, $hashed_password)){
+                        if($password == $hashed_password ){
                             // Password is correct, so start a new session
                             session_start();
                             
@@ -87,7 +87,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
                             // Bind result variables
                             $stmt->bind_result($id,$username, $hashed_password);
                             if($stmt->fetch()){
-                                if(password_verify($password ,$hashed_password)){
+                                if($password == $hashed_password){
                                     // Password is correct, so start a new session
                                     session_start();
                                     
